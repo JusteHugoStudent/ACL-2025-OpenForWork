5d1aa4dcf10c388a917b12a3d4ebf82cb18d0d18
Taqinou <enzo.gazzoli@icloud.com>
2025-10-10 17:09:55 +0200

    modif serveur : utilisation d'express

--- serveur.js
+++ serveur.js
@@ -1,35 +1,30 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const path = require('path');
 const connectToMongo = require('./mongo');
 
-connectToMongo().then(db => {
-  if (db) {
-    console.log('Connexion à MongoDB réussie au lancement du serveur.');
-  } else {
-    console.log('Échec de la connexion à MongoDB.');
-  }
+const app = express();
+const PORT = 4000;
+
+connectToMongo()
+  .then(db => {
+    if (db) {
+      console.log('Connexion à MongoDB réussie au lancement du serveur.');
+    } else {
+      console.log('Échec de la connexion à MongoDB.');
+    }
+  })
+  .catch(err => console.error('Erreur de connexion MongoDB :', err));
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/' || req.url === '/index.html') {
-    const filePath = path.join(__dirname, 'src', 'index.html');
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Erreur serveur : impossible de lire index.html');
-      } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      }
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
-  }
+app.use((req, res) => {
+  res.status(404).send('404 Not Found');
 });
 
-const PORT = 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
